23553d22db9a2ee440a375df681ab045f5a21ad5
ABHI <abhishek.jagtap@solveallsolutions.com>
2026-02-27 16:05:04 +0530

    Revert "guest componuts"
    This reverts commit 6fa54472d7cb1a639117ec790001410920c60472.

--- src/components/Header/Guest/Guest.tsx
+++ src/components/Header/Guest/Guest.tsx
@@ -5,7 +5,6 @@ import Countguestcard from "./Countguestcard";
 import { FaAngleRight } from "react-icons/fa6";
 interface GuestInterface {
   handleUpdateComp: () => void;
-  tab: any;
   setState: any;
   guest: number;
 }
@@ -13,7 +12,6 @@ export default function Guest({
   handleUpdateComp,
   setState,
   guest,
-  tab,
 }: GuestInterface) {
   return (
     <>
